f8b2eb2678c8d7dc999f487cef25231df43d0e42
Sundharesan <sundharesan.krishnan@trustrace.com>
2024-01-23 16:08:47 +0530

    DSA sorting

--- Bro_Code/src/sort/Selection_sort.java
+++ Bro_Code/src/sort/Selection_sort.java
@@ -1,5 +1,27 @@
 package sort;
 
-public class Selection_sort {
+import java.util.Arrays;
+import java.util.List;
 
+public class Selection_sort {
+    public static void main(String[] args) {
+        int array[] = {9, 2, 3, 1, 4, 8, 5};
+        selection_sort(array);
+        for (int nums : array){
+            System.out.print(nums);
+        }
+    }
+    private static void selection_sort(int[] array) {
+        for (int i = 0; i < array.length-1; i++) {
+            int min = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[min] > array[j]){
+                    min = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }
+    }
 }
